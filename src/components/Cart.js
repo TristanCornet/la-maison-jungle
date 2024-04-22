8862ae743e94c84184716ade4fa5937164362cf0
Tristan Cornet <tcornet74@hotmail.com>
2024-04-22 09:08:56 +0200

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import '../styles/Cart.css'
+
+function Cart({cart, updateCart}) {
+	const [isOpen, setIsOpen] = useState(true)
+
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
+	)
+
+	// useEffect(() => {
+	// 	alert(`J'aurai ${total}€ à payer 💸`)
+	// }, [total]) 
+
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+	}, [total])
+
+	
+	return isOpen ? (
+		<div className='lmj-cart'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(false)}
+			>
+				Fermer
+			</button>
+			<h2>Panier</h2>
+
+			{cart.map(({name, price, amount}, index) => (
+				<div key={`${name}-${index}`}>
+					{name} {price}€ x {amount}
+				</div>
+			))}
+
+			<h3>Total : {total}€</h3>
+			<button onClick={() => updateCart([])}>Vider le panier</button>
+		</div>
+	) : (
+		<div className='lmj-cart-closed'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(true)}
+			>
+				Ouvrir le Panier
+			</button>
+		</div>
+	)
+}
+
+export default Cart
